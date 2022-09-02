40368bb7209c0073ce1a7193906fc3b09843d7ad
黑山老妖 <hsly889@126.com>
2022-09-02 15:13:37 +0800

    测试配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,17 +1,18 @@
 export default {
     base: '/front-end_architecture',
-    title: 'VitePress',
+    title: '高级前端必学必会',
     description: 'Just playing around.',
     head: [],
     locales: {},
     themeConfig:{
         locales: {},
-        repo: 'vuejs/vitepress',
+        repo: 'DavisYao88/front-end_architecture',
         docsDir: 'docs',
-        editLinks: true,
-        editLinkText: 'Edit this page on GitHub',
         lastUpdated: 'Last Updated',
         nav: [],
-        sidebar: {  }
+        sidebar: [
+            { text: "低代码", link: "/lowcode/" },
+            { text: "工程化", link: "/engineering/" }
+        ]
     }
 }
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,31 @@
-# Hello VitePress
+---
+layout: home
+
+hero:
+  name: 东方小月的博客
+  text: 随便写点啥.
+  tagline: 帅气又迷人的小月
+  image:
+    src: /logo.png
+    alt: Kitty
+  actions:
+    - theme: brand
+      text: 快来快来
+      link: /articles/组件库环境搭建
+    - theme: alt
+      text: View on Gitee
+      link: https://gitee.com/geeksdidi
+
+features:
+  - icon: ⚡️
+    title: 这是一个闪电图标
+    details: wawawa
+  - icon: 🖖
+    title: 这是一个手掌图标
+    details: good...
+  - icon: 🛠️
+    title: 这是一个修理图标
+    details: cocococo
+---
+
+
