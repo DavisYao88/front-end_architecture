eeb4172da9f9626ae17854fb2542357fe26604fa
黑山老妖 <hsly889@126.com>
2022-09-02 14:31:58 +0800

    配置部署

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+    base: '/front-end_architecture',
+    title: 'VitePress',
+    description: 'Just playing around.',
+    head: [],
+    locales: {},
+    themeConfig:{
+        locales: {},
+        repo: 'vuejs/vitepress',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+        nav: [],
+        sidebar: {  }
+    }
+}
