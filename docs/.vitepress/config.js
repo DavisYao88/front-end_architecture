ab937ba39c9dcdab57827c4b124e4faede0f74df
黑山老妖 <hsly889@126.com>
2022-09-03 13:09:30 +0800

    添加文件

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,26 +1,9 @@
+import themeConfig from "./config/themeConfig"
 export default {
     base: '/front-end_architecture',
     title: '高级前端必学必会',
     description: 'Just playing around.',
     head: [],
     locales: {},
-    themeConfig:{
-        locales: {},
-        repo: 'DavisYao88/front-end_architecture',
-        docsDir: 'docs',
-        lastUpdated: 'Last Updated',
-        nav: [],
-        sidebar: [
-            { text: "低代码", link: "/lowcode/" },
-            { text: "工程化", link: "/engineering/" },
-            { text: "源码共读", link: "/sourcecode/" }
-        ],
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/DavisYao88/front-end_architecture.git' }
-        ],
-        footer: {
-            message: '',
-            copyright: 'Copyright © 2022-present '
-        }
-    }
+    themeConfig
 }
